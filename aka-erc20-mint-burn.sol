abc0a67c8b16700906a26a6a6461293b2c31d475
Frank P <frank.pira@escaroo.com>
2021-11-22 11:34:40 +0000

    Create aka-erc20-mint-burn.sol

---
+++ aka-erc20-mint-burn.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+ pragma solidity ^0.8.2;
+
+ /**
+  * @title ERC20 Token
+  * @author AKA Exchange
+  * @dev An ERC20 Token that is burnable, mintable, and has a max supply built
+  * using openzeppelin contracts.
+  */
+
+ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+ contract MyToken is ERC20, ERC20Burnable, Ownable {
+
+     /**
+      * @dev Set the max supply
+      */
+     uint256 private _maxSupply = 1000000000 * 18 ** decimals();
+
+     constructor() ERC20("AKA Exchange - Ethereum", "AKAE") {
+         /**
+          * @dev Set the premint amount
+          */
+         _mint(msg.sender, 2500000 * 18 ** decimals());
+     }
+
+     /**
+      * @dev Returns max supply of the token.
+      */
+      function getMaxSupply() public view returns (uint256) {
+          return _maxSupply;
+      }
+
+     /**
+      * @dev Only the owner will be able to mint tokens and ensures
+      * total supply doesn't go beyond the max supply
+      */
+      function mint(address to, uint256 amount) public onlyOwner {
+          /**
+           * @dev Total Spply + Mint Amount should not exceed Max Supply
+           */
+          require(totalSupply() + amount <= getMaxSupply(), "Max Supply Exceeded");
+          _mint(to, amount);
+      }
+ } 
