88919c23e76feed09b999aa49708ecd61b8062aa
Frank P <frank.pira@escaroo.com>
2021-11-22 11:36:59 +0000

    Update aka-erc20-mint-burn.sol

--- aka-erc20-mint-burn.sol
+++ aka-erc20-mint-burn.sol
@@ -8,9 +8,9 @@
   * using openzeppelin contracts.
   */
 
- import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
- import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Burnable.sol";
- import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+ import "@openzeppelin/contracts/access/Ownable.sol";
 
  contract MyToken is ERC20, ERC20Burnable, Ownable {
 
